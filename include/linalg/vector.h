7a2381f5cf3e3abbdf2581481b88297174948251
Hamza B <hbacc2nd@gmail.com>
2024-05-02 18:04:15 +0100

    added cmakelist for test

--- include/linalg/vector.h
+++ include/linalg/vector.h
@@ -1,5 +1,6 @@
 #ifndef VECTOR_H
 #define VECTOR_H
+
 #include <type_traits>
 
 namespace linalg
@@ -7,13 +8,16 @@ namespace linalg
 	template<typename T>
 	class Vector
 	{
+	public:
+		Vector operator+(const Vector& a);
+
+
+
 	private:
 		static_assert(std::is_arithmetic_v, "T must be a numeric type");
 		T data[];
 
 
-	public:
-		Vector operator+(const Vector& a);
 	};
 }
 
